c90cf4a38a0a7bb0fa7302540f4caeb3c4037040
ceedevgirl <ceciliaarthur347@gmail.com>
2025-05-02 02:33:29 +0000

    Refactor: implement room factory function

--- main.js
+++ main.js
@@ -1,146 +1,44 @@
-// Room objects
+// Room Factory Function
+function createRoom({ name, currTemp, coldPreset, warmPreset, image, startTime, endTime }) {
+  return {
+    name,
+    currTemp,
+    coldPreset,
+    warmPreset,
+    image,
+    airConditionerOn: false,
+    startTime,
+    endTime,
+
+    setCurrTemp(temp) {
+      this.currTemp = temp;
+    },
+    setColdPreset(temp) {
+      this.coldPreset = temp;
+    },
+    setWarmPreset(temp) {
+      this.warmPreset = temp;
+    },
+    increaseTemp() {
+      this.currTemp++;
+    },
+    decreaseTemp() {
+      this.currTemp--;
+    },
+    toggleAircon() {
+      this.airConditionerOn = !this.airConditionerOn;
+    },
+  };
+}
+
+// Rooms array
 const rooms = [
-  {
-    name: "Living Room",
-    currTemp: 32,
-    coldPreset: 20,
-    warmPreset: 32,
-    image: "./assets/living-room.jpg",
-    airConditionerOn: false,
-    startTime: '16:30',
-    endTime: '20:00',
-
-    setCurrTemp(temp) {
-      this.currTemp = temp;
-    },
-
-    setColdPreset(newCold) {
-      this.coldPreset = newCold;
-    },
-
-    setWarmPreset(newWarm) {
-      this.warmPreset = newWarm;
-    },
-
-    decreaseTemp() {
-      this.currTemp--;
-    },
-
-    increaseTemp() {
-      this.currTemp++;
-    },
-    toggleAircon() {
-      this.airConditionerOn
-        ? (this.airConditionerOn = false)
-        : (this.airConditionerOn = true);
-    },
-  },
-  {
-    name: "Kitchen",
-    currTemp: 29,
-    coldPreset: 20,
-    warmPreset: 32,
-    image: "./assets/kitchen.jpg",
-    airConditionerOn: false,
-    startTime: '16:30',
-    endTime: '20:00',
-
-    setCurrTemp(temp) {
-      this.currTemp = temp;
-    },
-
-    setColdPreset(newCold) {
-      this.coldPreset = newCold;
-    },
-
-    setWarmPreset(newWarm) {
-      this.warmPreset = newWarm;
-    },
-
-    decreaseTemp() {
-      this.currTemp--;
-    },
-
-    increaseTemp() {
-      this.currTemp++;
-    },
-    toggleAircon() {
-      this.airConditionerOn
-        ? (this.airConditionerOn = false)
-        : (this.airConditionerOn = true);
-    },
-  },
-  {
-    name: "Bathroom",
-    currTemp: 30,
-    coldPreset: 20,
-    warmPreset: 32,
-    image: "./assets/bathroom.jpg",
-    airConditionerOn: false,
-    startTime: '16:30',
-    endTime: '20:00',
-
-    setCurrTemp(temp) {
-      this.currTemp = temp;
-    },
-
-    setColdPreset(newCold) {
-      this.coldPreset = newCold;
-    },
-
-    setWarmPreset(newWarm) {
-      this.warmPreset = newWarm;
-    },
-
-    decreaseTemp() {
-      this.currTemp--;
-    },
-
-    increaseTemp() {
-      this.currTemp++;
-    },
-    toggleAircon() {
-      this.airConditionerOn
-        ? (this.airConditionerOn = false)
-        : (this.airConditionerOn = true);
-    },
-  },
-  {
-    name: "Bedroom",
-    currTemp: 31,
-    coldPreset: 20,
-    warmPreset: 32,
-    image: "./assets/bedroom.jpg",
-    airConditionerOn: false,
-    startTime: '16:30',
-    endTime: '20:00',
-
-    setCurrTemp(temp) {
-      this.currTemp = temp;
-    },
-
-    setColdPreset(newCold) {
-      this.coldPreset = newCold;
-    },
-
-    setWarmPreset(newWarm) {
-      this.warmPreset = newWarm;
-    },
-
-    decreaseTemp() {
-      this.currTemp--;
-    },
-
-    increaseTemp() {
-      this.currTemp++;
-    },
-    toggleAircon() {
-      this.airConditionerOn
-        ? (this.airConditionerOn = false)
-        : (this.airConditionerOn = true);
-    },
-  },
+  createRoom({ name: "Living Room", currTemp: 32, coldPreset: 20, warmPreset: 32, image: "./assets/living-room.jpg", startTime: "16:30", endTime: "20:00" }),
+  createRoom({ name: "Kitchen", currTemp: 29, coldPreset: 20, warmPreset: 32, image: "./assets/kitchen.jpg", startTime: "16:30", endTime: "20:00" }),
+  createRoom({ name: "Bathroom", currTemp: 30, coldPreset: 20, warmPreset: 32, image: "./assets/bathroom.jpg", startTime: "16:30", endTime: "20:00" }),
+  createRoom({ name: "Bedroom", currTemp: 31, coldPreset: 20, warmPreset: 32, image: "./assets/bedroom.jpg", startTime: "16:30", endTime: "20:00" }),
 ];
+    
 
 const warmOverlay= `linear-gradient(
     to bottom,
